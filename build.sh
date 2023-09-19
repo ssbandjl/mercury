bde8247f163c77fc2df517a6ebc36d041bb87392
xb <ssbandjl@163.com>
2023-09-19 17:50:05 +0800

    bulk

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 # centos7 open HG_HAS_BOOST, yum install -y boost-python36-devel
+###
+ # @Author: xb ssbandjl@163.com
+ # @Date: 2023-09-19 08:37:17
+ # @LastEditors: xb ssbandjl@163.com
+ # @LastEditTime: 2023-09-19 17:44:11
+ # @FilePath: /mercury/build.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 rm -rf build
 mkdir build
 cd build
